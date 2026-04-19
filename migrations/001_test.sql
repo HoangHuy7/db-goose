32b0b2136a091699096e8794133dee07c7c7ba7b
Huy Trần Nguyễn Hoàng <huytrannguyenhoang@Huys-MacBook-Pro.local>
2026-04-19 22:46:59 +0700

    first commit

---
+++ migrations/001_test.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS goose_test_ping
+(
+    id
+    SERIAL
+    PRIMARY
+    KEY,
+    pingED_at
+    TIMESTAMP
+    DEFAULT
+    NOW
+(
+)
+    );
+
+-- +goose Down
+DROP TABLE goose_test_ping;
