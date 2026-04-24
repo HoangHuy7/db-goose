5c968af358d97e6f61a45efdf7f4554ae3365016
Huy Trần Nguyễn Hoàng <huytrannguyenhoang@Huys-MacBook-Pro.local>
2026-04-25 00:21:58 +0700

    Update CI workflow, IDE settings, and migration

--- migrations/001_test.sql
+++ migrations/001_test.sql
@@ -1,17 +1,9 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS goose_test_ping
 (
-    id
-    SERIAL
-    PRIMARY
-    KEY,
-    pingED_at
-    TIMESTAMP
-    DEFAULT
-    NOW
-(
-)
-    );
+    id SERIAL PRIMARY KEY,
+    pinged_at TIMESTAMP DEFAULT NOW()
+);
 
 -- +goose Down
 DROP TABLE goose_test_ping;
